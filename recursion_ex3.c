ac74350d48fb70c777ed407946a08006587bd536
PRAKYATH L V <74598370+prakyathlv@users.noreply.github.com>
2021-09-03 09:18:59 +0530

    finding exponents using recursion

---
+++ recursion_ex3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+
+	int power(int m,int n)
+	{
+		if(n==0)
+		return 1;
+		if(n%2==0)
+		return power(m*m,n/2);
+		else
+		return m*power(m*m,(n-1)/2);
+	}   
+	
+	int main()
+	{
+		int a=power(2,9);
+		printf("%d",a);
+	}
